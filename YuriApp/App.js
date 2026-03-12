464a2ade6b936462c34b68851eabd28bd7a3c529
yvos77 <yuripb902@gmail.com>
2026-03-11 21:05:56 -0300

    Feat: Finaliza a implementação da Prática 01

--- YuriApp/App.js
+++ YuriApp/App.js
@@ -1,53 +1,47 @@
-{/*import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import { rotulo_btn_cadastro_meta, rotulo_input_meta, rotulo_lista_metas } from './mensagens.js';
 
 export default function App() {
   return (
     <View style={styles.mainContainer}>
-      <TextInput placeholder={rotulo_input_meta}/>
-      <Button title={rotulo_btn_cadastro_meta} />
-      <Text>{rotulo_lista_metas}</Text>
-      <StatusBar style="auto" />
-
+      <View>
+        <View style={styles.formContainer}>
+          <TextInput style={styles.inputText} placeholder={rotulo_input_meta}/>
+        </View>
+        <View>
+          <Button title={rotulo_btn_cadastro_meta} />
+        </View>
+      </View>
+      <View>
+        <Text style={styles.listaTexto}>{rotulo_lista_metas}</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+
+  mainContainer: {
     flex: 1,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 30,
+    paddingTop: 60,
+    backgroundColor: '#fff',
   },
-  mainContainer: {
+
+  formContainer: {
+    flexDirection: 'row',
+    gap: 10
+  },
+
+  inputText: {
     flex: 1,
-    padding: 30,
-    backgroundColor: '#ffffff',
+    borderColor: '#ccccc',
+    borderWidth: 1,
+    padding: 10
+  },
+
+  listaTexto: {
+    marginTop: 20
   }
-});
-*/}
 
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, SafeAreaView } from 'react-native-safe-area-context';
-import { rotulo_btn_cadastro_meta, rotulo_input_meta, rotulo_lista_metas } from './mensagens.js';
-
-export default function App() {
-  return (
-    <SafeAreaView style={styles.mainContainer}>
-      <TextInput placeholder={rotulo_input_meta}/>
-      <Button title={rotulo_btn_cadastro_meta} />
-      <Text>{rotulo_lista_metas}</Text>
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  mainContainer: {
-    flex: 1,
-    padding: 30,
-    backgroundColor: '#ffffff',
-  }
 });
