326e1c527647231500063e52b112c6a099316cd6
yvos77 <yuripb902@gmail.com>
2026-05-09 17:27:35 -0300

    Códigos da parte 6 do projeto

--- praticas/gestao-financeira/styles/globalStyles.js
+++ praticas/gestao-financeira/styles/globalStyles.js
@@ -23,5 +23,27 @@ export const globalStyles = StyleSheet.create({
     fontSize: 16,
     color: colors.primaryText,
     marginBottom: 4
-  }
+  },
+  line: {
+    backgroundColor: colors.secondaryText,
+    height: 1,
+    opacity: 0.5,
+    marginBottom: 4,
+  },
+  primaryText: {
+    fontSize: 16,
+    color: colors.primaryText,
+  },
+  secondaryText: {
+    fontSize: 12,
+    color: colors.secondaryText,
+  },
+  positiveText: {
+    fontSize: 16,
+    color: colors.positiveText,
+  },
+  negativeText: {
+    fontSize: 16,
+    color: colors.negativesText,
+  },
 })
